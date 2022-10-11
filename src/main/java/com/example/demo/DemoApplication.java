0d021f32b3bf962c31750a0478f73a3bce1c495a
Ghazi Ladjimi <ghazi.ladjimi1@littlebigconnection.com>
2022-10-11 09:40:48 +0100

    Bank Account Kata

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,11 +12,16 @@ public class DemoApplication {
   public static void main(String[] args) throws NegativeBalanceException {
     SpringApplication.run(DemoApplication.class, args);
 
+    // create an Account
     Account account = new Account(500);
 
+    // Deposit 100 to the account
     account.deposit(100);
+
+    //Withdraw 400 from the account
     account.withdraw(400);
 
+    // Print the history of my operations
     System.out.println(account.printStatement());
   }
 
